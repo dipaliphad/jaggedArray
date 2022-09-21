260a941f088d0792a1c8da0d6617cc689789d97b
rupesh palve <rupesh.palve48@outlook.com>
2022-09-21 16:00:37 -0400

    Add project files.

---
+++ jaggedArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jaggedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggeArray = new int[3][];
+
+            jaggeArray[0] = new int[] { 1, 2, 3, 4, 5 };
+            jaggeArray[1] = new int[] { 1, 2, 3 };
+            jaggeArray[2] = new int[] { 1, 2 };
+
+            for(int i=0; i<jaggeArray.Length; i++)
+            {
+                for(int j=0; j<jaggeArray[i].Length; j++)
+                {
+                    Console.Write(jaggeArray[i][j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
